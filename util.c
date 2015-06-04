6928524c6c66935c2a4ce4f877db0082297fab91
Claudio Matsuoka <cmatsuoka@gmail.com>
2015-06-04 10:40:47 -0300

    Add fopen wrapper to also check uppercase filenames
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- util.c
+++ util.c
@@ -1,5 +1,8 @@
 
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "common.h"
 
 int read16l(FILE *f)
@@ -12,3 +15,30 @@ int read16l(FILE *f)
 	return (b << 8) | a;
 }
 
+FILE *fopen_wrapper(char *filename, char *mode)
+{
+	FILE *f;
+	char *u;
+	int i, len;
+
+	if ((f = fopen(filename, mode)) != NULL) {
+		return f;
+	}
+
+	if ((u = strdup(filename)) == NULL) {
+		return NULL;
+	}
+
+	len = strlen(filename);
+	for (i = 0; i < len; i++) {
+		u[i] = toupper(filename[i]);
+	}
+
+	if ((f = fopen(u, mode)) != NULL) {
+		free(u);
+		return f;
+	}
+
+	free(u);
+	return NULL;
+}
